4e226c8ea4a9c511b81f99108659c481e7f937c5
Jose Garcia <jose.d.garcia@asu.edu>
2013-03-25 16:00:48 -0700

    added a css folder/file, and few login page changes

--- index.php
+++ index.php
@@ -1,30 +1,39 @@
-<html>
- 	<head>
- 		<title>Login</title>
- 	<head>
- <body>
+<!doctype html>
+<html lang="en">
+    <head>
+        <title>Well-Check Clinic | Login</title>
+        <link rel="stylesheet" href="css/style.css">
+    <head>
 
- <form method = "post" action = "checklogin.php">
- <table>
- 	<tr>
- 		<td>Association:</td>
- 		<td><select name="association">
- 				<option value="Admins">-Select-</option>
- 				<option value="Doctors">Doctor</option>
- 				<option value="Nurses">Nurse</option>
- 				<option value="Patients">Patient</option>
- 			</select></td>
- 	</tr>
- 	<tr>
-		 <td>Username:</td>
-		 <td><input type="text" name="username"></td>
-	</tr>
-	<tr>
-		<td>Password:</td>
-		<td><input type="password" name="password"></td>
- 	<tr>
- 		<td><input type="submit" name="submit" value="Login"></td>
- </table>
-</form>
-</body>
+    <body>
+        <h1>Welcome to Well-Check Clinic</h1>
+        <form method="post" action="checklogin.php">
+        <table>
+            <tr>
+                <td>Association:</td>
+                <td>
+                    <select name="association">
+                        <option value="Doctors">Doctor</option>
+                        <option value="Nurses">Nurse</option>
+                        <option value="Patients">Patient</option>
+                        <option value="Admins">Admin</option>
+                    </select>
+                </td>
+            </tr>
+            <tr>
+                <td>Username:</td>
+                <td><input type="text" name="username"></td>
+            </tr>
+            <tr>
+                <td>Password:</td>
+                <td><input type="password" name="password"></td>
+            </tr>
+            <tr>
+                <td><input type="submit" name="submit" value="Login"></td>
+                <!-- THIS CURRENTLY GOES NOWHERE -->
+                <td><a class="small" href="">Forgot username or password?</a></td>
+            </tr>
+        </table>
+        </form>
+    </body>
 </html>
