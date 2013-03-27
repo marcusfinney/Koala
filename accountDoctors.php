ff0ff5e226a36b97f5cb2080d345635fea8581cc
Jose Garcia <jose.d.garcia@asu.edu>
2013-03-27 01:09:09 -0700

    added patient select

--- accountDoctors.php
+++ accountDoctors.php
@@ -9,20 +9,39 @@
         <?php
             session_start();
             echo "<h1>Dr. {$_SESSION["userrecord"]["firstname"]} {$_SESSION["userrecord"]["lastname"]}</h1>";
-        ?>
 
-        <!-- need to create page for patient info -->
-        <form method="post" action="">
-            Select a patient: 
-            <select name="Patients">
-                <!-- need to access all patients a doctor has and put them here -->
-                <option>placeholder</option>
-            </select>
-        </form>
+            include 'config.php';
+            mysql_connect($host, $user, $password) or die("cant connect");
+            mysql_select_db($database) or die(mysql_error());
+
+            $myID = $_SESSION["userrecord"]["iddoctor"];
+
+            $sql = "SELECT idpatient, firstname, lastname
+                    FROM Patients
+                    WHERE iddoctor={$myID}";
+            $mypatients = mysql_query($sql);
+            
+            $numberofpatients = mysql_num_rows($mypatients);
+
+            if ($numberofpatients == 0) {
+                echo '<p>You have no registered patients.</p>';
+            }
+            else {
+                // ADD THE PAGE FOR VIEWING PATIENT DATA IN THE ACTION FIELD OF NEXT LINE
+                echo '<form method="post" action="">
+                        <select name="Patients">';
+                while ($row = mysql_fetch_assoc($mypatients)) {
+                    echo  "<option value={$row['idpatient']}>{$row['lastname']}, {$row['firstname']}</option>";
+                }
+                echo '  </select>
+                        <input type="submit" name="submit" value="Select Patient">
+                      </form>';
+            }
+        ?>
 
         <p><a href="register.php"><button>Register a Patient</button></a></p>
 
-        <p><a href="logout.php">Logout</a></p>
+        <p><a href="logout.php"><button>Logout</button></a></p>
 
     </body>
 </html>
