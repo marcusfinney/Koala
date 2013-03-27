5ea231caf0d7fc2e6dfac1f522522a6e1b20e2be
Jose Garcia <jose.d.garcia@asu.edu>
2013-03-26 22:52:47 -0700

    incorrect login message now shows on login page

--- checklogin.php
+++ checklogin.php
@@ -1,5 +1,5 @@
 <?php
- ob_start(); 
+ob_start(); 
 
 session_start();
 
@@ -9,36 +9,42 @@ include 'config.php';
 mysql_connect($host, $user, $password) or die("cant connect");
 mysql_select_db($database) or die(mysql_error());
 
-$usertable=$_POST["association"];
-$myusername=$_POST["username"];
-$mypassword=$_POST["password"];
+$usertable  = $_POST["association"];
+$myusername = $_POST["username"];
+$mypassword = $_POST["password"];
 
 
-$sql="SELECT * FROM {$usertable} WHERE username='{$myusername}' AND password='{$mypassword}'";
-$result=mysql_query($sql);
+$sql = "SELECT * FROM {$usertable} WHERE username='{$myusername}' AND password='{$mypassword}'";
+$result = mysql_query($sql);
 
-$count=mysql_num_rows($result);
+$count = mysql_num_rows($result);
 
-if($count == 1){
-	$_SESSION["username"] = $myusername;
-	$_SESSION["password"] = $mypassword;
-	$_SESSION["userrecord"] = mysql_fetch_assoc($result);
-	if($usertable == 'Doctors')
-	{header("location:accountDoctors.php");}
-	elseif($usertable == 'Nurses')
-	{header("location:accountNurses.php");}
-	elseif($usertable == 'Patients')
-	{header("location:accountPatients.php");}
-	else
-	{header("location:accountAdmins.php");}}
-else{echo "<script type='text/javascript'>alert('Incorrect username/password');</script>";}
+if ($count == 1) {
+    $_SESSION["username"] = $myusername;
+    $_SESSION["password"] = $mypassword;
+    $_SESSION["userrecord"] = mysql_fetch_assoc($result);
+    if($usertable == 'Doctors')
+    {header("location:accountDoctors.php");}
+    elseif($usertable == 'Nurses')
+    {header("location:accountNurses.php");}
+    elseif($usertable == 'Patients')
+    {header("location:accountPatients.php");}
+    else
+    {header("location:accountAdmins.php");}
+}
+else{
+    // return user to login page and mark page with login error
+    echo "<script type='text/javascript'>alert('Incorrect username/password');</script>";
+    header("location:index.php?error=incorrectlogin");
+}
 ?>
-<html>
+
+<!-- <html>
 	<head>
 		<title></title>
 	</head>
 <body>
 <br>
-<!--Go back to <a href="index.php">login</a> page.-->
+Go back to <a href="index.php">login</a> page.
 </body>
-</html>
+</html> -->
--- index.php
+++ index.php
@@ -7,6 +7,12 @@
 
     <body>
         <h1>Welcome to Well-Check Clinic</h1>
+        <!-- displays error message if checklogin.php did not pass login -->
+        <?php
+            if ($_GET and $_GET["error"] == "incorrectlogin") {
+                echo '<p class="error">Incorrect username and/or password.</p>';
+            }
+        ?>
         <form method="post" action="checklogin.php">
         <table>
             <tr>
