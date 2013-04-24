f4b169c1d8cf3840163649c1f1306bbb489e71aa
Ryan Kenney <ultimatewpn@gmail.com>
2013-04-24 15:28:26 -0700

    more link fixing

--- editInfo.php
+++ editInfo.php
@@ -58,7 +58,7 @@ else
                 <li><a href="vitalm.php">Vitals</a></li>
                 <li><a href="account<?php echo $_SESSION["accountType"];?>.php">Notes</a></li>
                 <li><a href="account<?php echo $_SESSION["accountType"];?>.php">Messages</a></li>
-                <li><a href="account<?php echo $_SESSION["accountType"];?>.php">Prescriptions</a></li>
+                <li><a href="prescriptionPage.php">Prescriptions</a></li>
                 <li class="active"><a href="editInfo.php">Edit Info</a></li>
             </ul>
 
